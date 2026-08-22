3e3232cb584711b7bb2c309c7c486b93ff8cbd7e
kayke <kaykecristofersc@gmail.com>
2026-08-22 17:19:43 -0300

    feat: cria a aba para indicadores gerais por quadriênio

--- src/data/data.js
+++ src/data/data.js
@@ -5,6 +5,7 @@ export const navigationLinks = [
     { id: 1, title: 'Home', image: iconsImgs.home , path: '/dashboard'},
     { id: 2, title: 'Gráficos', image: iconsImgs.chart , path: '/graficos'},
     { id: 3, title: 'Currículos', image: iconsImgs.chart , path: '/curriculos'},
-    { id: 4, title: 'Rede de Coautoria', image: iconsImgs.user, path: '/rede-coautoria'},
-    { id: 5, title: 'Settings', image: iconsImgs.gears , path: '#'}
+    { id: 4, title: 'Produção Docente', image: iconsImgs.chart, path: '/producao-docente'},
+    { id: 5, title: 'Rede de Coautoria', image: iconsImgs.user, path: '/rede-coautoria'},
+    { id: 6, title: 'Settings', image: iconsImgs.gears , path: '#'}
 ];
